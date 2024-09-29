a422f8b66493d2097091d6455f4edae688d5f2b6
Rohit Sahu <sahu86744@gmail.com>
2024-09-29 16:58:45 +0530

    added api

--- pages/api/cart.js
+++ pages/api/cart.js
@@ -1,16 +1,29 @@
+import { cors, runMiddleware } from "@/lib/cors";
 import { mongooseConnect } from "@/lib/mongoose";
 import { Product } from "@/models/Product";
 
+
+
 export default async function handle(req, res) {
+  await runMiddleware(req, res, cors);
   await mongooseConnect();
   if (req.method === 'POST') {
-    const ids = req.body.ids;
-    if (ids.length === 0) {
+    let { ids } = req.body;
+    ids = JSON.parse(ids);
+    console.log('Received ids:', ids);
+
+    if (!Array.isArray(ids) || ids.length === 0) {
       return res.status(400).send('No ids provided');
     }
-    const products = await Product.find({ _id: { $in: ids } });
-    res.status(200).send(products);
+
+    try {
+      const products = await Product.find({ _id: { $in: ids } });
+      res.status(200).send(products);
+    } catch (error) {
+      console.log('Error fetching products from database:', error);
+      res.status(500).send('Error fetching products');
+    }
   } else {
-    res.status(405).send('Method not allowed');
+    res.status(405).send(`Method ${req.method} Not Allowed`);
   }
 }
